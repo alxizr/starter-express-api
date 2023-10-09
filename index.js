62e4d5284fcc85dd129657aadfa575ca9923de5a
alxizr <39420735+alxizr@users.noreply.github.com>
2023-10-09 16:04:40 +0300

    Update index.js
    create api/receivesms API endpoint

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+
+app.post("/api/receivesms", async (req, res, next)=>{
+    try {
+        const body = await req.json()
+        return res.status(200).json({body})
+    }
+    catch (err) {
+        return res.status(500).json({
+            success: false,
+            msg: "some error occured"
+        })
+    }
+})
+
 app.listen(process.env.PORT || 3000)
